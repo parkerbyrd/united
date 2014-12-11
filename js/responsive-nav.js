cd903e38d1461fb4ba6b22495ece116b2b12b692
parkerbyrd <parkerabyrd@gmail.com>
2014-12-11 10:53:28 -0500

    beginning forms

--- js/responsive-nav.js
+++ js/responsive-nav.js
@@ -144,8 +144,8 @@
         this.options = {
           animate: true,                    // Boolean: Use CSS3 transitions, true or false
           transition: 284,                  // Integer: Speed of the transition, in milliseconds
-          label: "Menu",                    // String: Label for the navigation toggle
-          insert: "before",                 // String: Insert the toggle before or after the navigation
+          label: "",                    // String: Label for the navigation toggle
+          insert: "after",                 // String: Insert the toggle before or after the navigation
           customToggle: "",                 // Selector: Specify the ID of a custom toggle
           closeOnNavClick: false,           // Boolean: Close the navigation when one of the links are clicked
           openPos: "relative",              // String: Position of the opened nav, relative or static
